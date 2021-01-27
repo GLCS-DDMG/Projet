e6fc3fb0c5fc67719e379e8fc2f2a6b27399deb7
Marius Ghertescu <m.ghertescu@gmail.com>
2021-01-27 12:43:41 +0100

    Config line command

--- Provided_src/simpleui/CMakeFiles/simpleui.dir/DependInfo.cmake
+++ Provided_src/simpleui/CMakeFiles/simpleui.dir/DependInfo.cmake
@@ -4,7 +4,6 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/data/simpleui/src/commandlineconfig.cpp" "/data/simpleui/CMakeFiles/simpleui.dir/src/commandlineconfig.cpp.o"
   "/data/simpleui/src/screenprinter.cpp" "/data/simpleui/CMakeFiles/simpleui.dir/src/screenprinter.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
